d9472d5d12cad869a943d6ff79558430ed141469
Pup Natchapol Watthanawong <pupreez@gmail.com>
2023-04-18 00:03:50 +0700

    gpt images

--- gpttest.py
+++ gpttest.py
@@ -1,6 +1,11 @@
 import openai
 import os
 from dotenv import load_dotenv
+from PIL import Image
+import requests
+from io import BytesIO
+
+
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
@@ -26,6 +31,42 @@ def get_reply(prompt, model="text-davinci-002", max_tokens=50):
     return message
 
 
-input_string = input("Prompt me:")
-reply = get_reply(input_string)
-print(reply)
+def generate_image(prompt):
+        response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+
+        return image_url
+
+def try_message():
+    input_string = input("Prompt me:")
+    reply = get_reply(input_string)
+    print(reply)
+     
+def try_image():
+    input_string = input("Prompt me:")
+    reply = generate_image(input_string)
+    # URL of the generated image
+    url = reply
+
+    # Send a GET request to the URL and get the response
+    response = requests.get(url)
+
+    # Open the image using Pillow's Image module
+    img = Image.open(BytesIO(response.content))
+
+    # Save the image to a directory
+    img.save("./images/test_image.jpg")
+    img.show()
+     
+     
+def main():
+   try_image()
+#    try_message()
+
+if __name__ == "__main__":
+    main()
+
